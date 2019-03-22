7c7f3a733f03128de0e81f9797089e86e1dc6308
pwachira900@gmail.com <pwachira900@gmail.com>
2019-03-22 12:05:58 +0300

    removed dist/ from gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {Goal} from './goal'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'Goals';
+    goals = [
+        new Goal(1,'Watch Finding Nemo','goal 1',new Date(2019,10,10)),
+        new Goal(2,'Buy Cookies','goal 2',new Date(2019,10,10)),
+        new Goal(3, 'Get new Phone Case','goal3',new Date(2019,10,10)),
+        new Goal(4, 'Get Dog Food','goal 4',new Date(2019,10,10)),
+        new Goal(5, 'Solve math homework','goal 5',new Date(2019,10,10)),
+        new Goal(6, 'Plot my world domination plan','goal 6',new Date(2019,10,10)),
+
+    ]
 }
